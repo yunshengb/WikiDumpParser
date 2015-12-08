a124c96cdce3edffb2f3d5ff1468a479e734cbae
yba <yba@umich.edu>
2015-12-08 01:19:39 -0500

    1208 1am

---
+++ src/Parse.java
@@ -0,0 +1,26 @@
+import edu.jhu.nlp.wikipedia.*; // download from https://code.google.com/p/wikixmlj/
+
+public class Parse {
+	public static void main(String[] args) {
+		System.out.println(args[0]);
+		WikiXMLParser wxsp = WikiXMLParserFactory.getSAXParser(args[0]);
+		try {
+
+			wxsp.setPageCallback(new PageCallbackHandler() { 
+				public void process(WikiPage page) {
+					if (!page.isRedirect()) {
+						
+						System.out.println("***********************************");
+						System.out.println("title: " + page.getTitle());
+						System.out.println(page.getWikiText());
+					}
+				}
+			});
+
+			wxsp.parse();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
